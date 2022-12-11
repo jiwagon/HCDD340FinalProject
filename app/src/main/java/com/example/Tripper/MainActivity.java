c0708311696a1221c29a21c3f4a9c3df6cafc17c
jiwagon <jiwagon11@gmail.com>
2022-12-11 18:10:41 -0500

    created homepage widget cardviews

--- app/src/main/java/com/example/Tripper/MainActivity.java
+++ app/src/main/java/com/example/Tripper/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.Tripper;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,10 +10,12 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, BottomNavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = "Activity 1";
 
@@ -20,15 +23,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView v = findViewById(R.id.bottom_navigation);
+        v.setOnNavigationItemSelectedListener(this);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.bottom_navigation_menu, menu);
-        return true;
-    }
+     @Override
+     public boolean onCreateOptionsMenu(Menu menu) {
+     getMenuInflater().inflate(R.menu.bottom_navigation_menu, menu);
+     return true;
+     }
 
-    @Override
+     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int menuId = item.getItemId();
 
@@ -37,8 +43,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent MainIntent = new Intent(this, MainActivity.class);
             startActivity(MainIntent);
             return true;
-        }
-        else if (menuId == R.id.bucket_list_label) {
+        } else if (menuId == R.id.bucket_list_label) {
             Log.d(TAG, "Bucket list page clicked");
             Intent bucketListIntent = new Intent(this, BucketListActivity.class);
             startActivity(bucketListIntent);
@@ -48,10 +53,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    public void onClick(View view) {
-
+    public void onClick(View v) {
 
     }
 
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.bucket_list_label) {
+            Intent bucketListIntent = new Intent(this, BucketListActivity.class);
+            startActivity(bucketListIntent);
+            return true;
+        } else {
+            Log.d(TAG, "Unknown item id in bottom nav!");
+            return true;
+        }
+        // return false;
+    }
 }
